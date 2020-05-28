f2bd5f40d5936da7d8879eb657fbe00d42837f2d
Aawaj Joshi <s521315@nwmissouri.edu>
2020-05-28 00:07:46 -0500

    Complete main page

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,18 @@
 import React from "react";
+import useSound from 'use-sound';
+
 import logo from '../assets/logo.png';
-
-
-
+import powerIcon from '../assets/power.svg';
+import keypress from '../assets/keypress.wav';
 
 function Main() {
+
+    const [play] = useSound(keypress);
+
     return (
         <div id="container">
             <img className="logo" src={logo} alt="logo" />
+            <button className="power-btn" onClick={play}><img src={powerIcon} alt="POWER"/></button> 
         </div>
     
     )
