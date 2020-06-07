37085b23fd9148079474951c977c8b4c0342dd97
Aawaj Joshi <s521315@nwmissouri.edu>
2020-06-07 00:56:26 -0500

    Updates

---
+++ precache-manifest.fa89444e3d2d4932794b0bbc487195e5.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81c78610a9ea29e27426453cb2b88126",
+    "url": "/adios/index.html"
+  },
+  {
+    "revision": "9f85c482b4285417805d",
+    "url": "/adios/static/js/10.2c377776.chunk.js"
+  },
+  {
+    "revision": "1129a1114cc21846a9b7",
+    "url": "/adios/static/js/11.40023f41.chunk.js"
+  },
+  {
+    "revision": "2a9334726112865bfc41",
+    "url": "/adios/static/js/12.ab216219.chunk.js"
+  },
+  {
+    "revision": "faebcd3696da64b9282b",
+    "url": "/adios/static/js/13.a9676dbb.chunk.js"
+  },
+  {
+    "revision": "7bb32364d375080f8aab",
+    "url": "/adios/static/js/14.e6bdf0c2.chunk.js"
+  },
+  {
+    "revision": "bb4336370de12b98e892",
+    "url": "/adios/static/js/15.90f1d6da.chunk.js"
+  },
+  {
+    "revision": "eae813b4543c7c217489",
+    "url": "/adios/static/js/2.d3e18b49.chunk.js"
+  },
+  {
+    "revision": "60bc296800dc943ea472d3266fbadc15",
+    "url": "/adios/static/js/2.d3e18b49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27a9db5968843ba48d58",
+    "url": "/adios/static/js/3.f6d850ab.chunk.js"
+  },
+  {
+    "revision": "287e48aeac3564a8b2b5685a67fb9216",
+    "url": "/adios/static/js/3.f6d850ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b830ae74823442a88c15",
+    "url": "/adios/static/js/4.45910d19.chunk.js"
+  },
+  {
+    "revision": "47b29b9101bd7fb6af60",
+    "url": "/adios/static/js/5.0aee0a36.chunk.js"
+  },
+  {
+    "revision": "8d1adaceb623e2f0e543",
+    "url": "/adios/static/js/6.b50f0e9b.chunk.js"
+  },
+  {
+    "revision": "a013329e313476a05c2e",
+    "url": "/adios/static/js/7.80c081f7.chunk.js"
+  },
+  {
+    "revision": "9c0c5ac0d2dbf5881965",
+    "url": "/adios/static/js/8.98e23b8f.chunk.js"
+  },
+  {
+    "revision": "ffffe85b029450247db3",
+    "url": "/adios/static/js/9.2fa19363.chunk.js"
+  },
+  {
+    "revision": "5fd7e0139a96f5df6ccb",
+    "url": "/adios/static/js/main.7b5e847e.chunk.js"
+  },
+  {
+    "revision": "55f707e328aa61c36963",
+    "url": "/adios/static/js/runtime-main.f4acec34.js"
+  },
+  {
+    "revision": "a7e29d6623aabaff40a1dce2141e0ccc",
+    "url": "/adios/static/media/bootup.a7e29d66.mp4"
+  },
+  {
+    "revision": "1317fe440a26aadadf30d94edd5a9525",
+    "url": "/adios/static/media/document.1317fe44.ico"
+  },
+  {
+    "revision": "d2636f3d0f9be1c241e601fd9891a3f4",
+    "url": "/adios/static/media/folder.d2636f3d.ico"
+  },
+  {
+    "revision": "30956c17fa5f3b3efd999fb1f0c5e145",
+    "url": "/adios/static/media/grace.30956c17.wav"
+  },
+  {
+    "revision": "5c4c9bbdadc9734a21e29353c5cfb5e5",
+    "url": "/adios/static/media/keypress.5c4c9bbd.wav"
+  },
+  {
+    "revision": "eaef46c21005f1a91c2a21fae887b508",
+    "url": "/adios/static/media/logo.cb8993af.png"
+  },
+  {
+    "revision": "2c56ce4c1e04e09f8fe9b3199279c03e",
+    "url": "/adios/static/media/logoff.2c56ce4c.mp3"
+  },
+  {
+    "revision": "8683e84be9ba48c32daf5604877c77d9",
+    "url": "/adios/static/media/logout.8683e84b.ico"
+  },
+  {
+    "revision": "49b127f4908032f654994745b112c826",
+    "url": "/adios/static/media/meme.49b127f4.mp3"
+  },
+  {
+    "revision": "2f0eda5a4b9eecbe9ba8837ea4cb69f8",
+    "url": "/adios/static/media/news.d98c5474.png"
+  },
+  {
+    "revision": "b062d1ba1667369dccf0460679195846",
+    "url": "/adios/static/media/power.b062d1ba.svg"
+  },
+  {
+    "revision": "6838015887edc0af64e8e580424ebc7c",
+    "url": "/adios/static/media/recycle-bin.68380158.ico"
+  },
+  {
+    "revision": "464f9170147956aed3c3090e9876dda4",
+    "url": "/adios/static/media/rick.dc7a25cf.gif"
+  },
+  {
+    "revision": "cc11aae412ec32c197eb5d0f69bf82c6",
+    "url": "/adios/static/media/start.cc11aae4.ico"
+  },
+  {
+    "revision": "57fd536499cc354aface4586f883fe05",
+    "url": "/adios/static/media/startup.57fd5364.mp3"
+  },
+  {
+    "revision": "b6ad8d749cb6ad704bc4a399ff33f84d",
+    "url": "/adios/static/media/user.b6ad8d74.ico"
+  },
+  {
+    "revision": "8e006b686d1c2f343fa2a1db4eb16b9f",
+    "url": "/adios/static/media/winamp.8e006b68.ico"
+  }
+]);
